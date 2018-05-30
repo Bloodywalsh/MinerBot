6038f489bcf02dde8fc1d66d6e3d390d083f498b
Bloody <josh.verespy@gmail.com>
2018-05-30 16:57:06 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+import random
 
+Client = discord.Client()
+client = commands.Bot(command_prefix = "!")
+
+
+@client.event
+async def on_ready():
+    print("active")
+
+@client.event
+async def on_message(message):
+    if message.content.upper().startswith('!VER'):
+        await client.send_message(message.channel, "**Ver 1.0.1**")
